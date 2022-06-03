42810ee6bdad0c4222203468f85ceee1ee01b040
Hakeem <64858288+Super-Nim@users.noreply.github.com>
2022-06-03 19:26:11 +0100

    Added Wallet component with helper functions - need to fix typescript errors and implement core functionality still

--- frontend/web/minter-dapp/src/components/FiatOnRamp.tsx
+++ frontend/web/minter-dapp/src/components/FiatOnRamp.tsx
@@ -60,7 +60,7 @@ const FiatOnRamp = () => {
   let key = 0;
   return (
     <div>
-      <h1 style={styles.title}>💸Native Transactions</h1>
+      <h1 style={styles.title}>💸Fiat Transactions</h1>
       <Skeleton
         loading={!nativeTransactions || nativeTransactions.length === 0}
       >
---
+++ frontend/web/minter-dapp/src/scss/HeaderStyles.ts
@@ -0,0 +1,32 @@
+const HeaderStyles = {
+    content: {
+      display: "flex",
+      justifyContent: "center",
+      fontFamily: "Roboto, sans-serif",
+      color: "#041836",
+      marginTop: "130px",
+      padding: "10px",
+    },
+    header: {
+      position: "fixed",
+      zIndex: 1,
+      width: "100%",
+      background: "#fff",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      fontFamily: "Roboto, sans-serif",
+      borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+      padding: "0 10px",
+      boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+    },
+    headerRight: {
+      display: "flex",
+      gap: "20px",
+      alignItems: "center",
+      fontSize: "15px",
+      fontWeight: "600",
+    },
+  };
+
+  export default HeaderStyles
