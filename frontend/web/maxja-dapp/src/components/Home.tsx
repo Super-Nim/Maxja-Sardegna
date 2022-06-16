e03ccf2092c2ea00cee77211081808a5265946de
Hakeem <64858288+Super-Nim@users.noreply.github.com>
2022-06-16 20:02:08 +0100

    home component added, need to add routing to buttons

--- frontend/web/maxja-dapp/src/components/Home.tsx
+++ frontend/web/maxja-dapp/src/components/Home.tsx
@@ -1,20 +1,50 @@
 import { Card, CardHeader, CardMedia, Grid, CardActions, CardContent, Button } from "@mui/material";
 import { Box } from "@mui/system";
+import { Typography } from "antd";
+import bigBuyCrypto from "../assets/bigBuyCrypto.png";
+import bigMintNFT from "../assets/bigMintNFT.png";
+import bigViewNFTs from "../assets/bigViewNFTs.png";
 
 const Home = () => {
 
-    const cardProps = [
+  type cardProps = {
+    title: string,
+    image: string,
+    description: string,
+    background: string,
+    buttonText: string
+  }
+
+    const cards: cardProps[] = [
         {
-            title: "Buy Crypto",
-            image: "",
+            title: "BUY CRYPTO",
+            image: bigBuyCrypto,
+            background: "#741918",
             description: "Get some Funds! /n Purchase at least 1 Matic and $100 USDC to MINT your NFT",
-            buttonText: "Buy Crypto"
+            buttonText: "BUY CRYPTO"
+        },
+        {
+          title: "MINT NFT",
+          image: bigMintNFT,
+          description: "This NFT is your Ticket to Maxja 2022 also gives you access to a % of discount for accommodation at the Venue!",
+          background: "",
+          buttonText: "MINT NFT"
+        },
+        {
+          title: "VIEW NFTs",
+          image: bigViewNFTs,
+          description: "“The Deer” Deer have sharp hearing, vision, and sense of smell. They can see approximately 360 degrees. Because of their potent sensitivity, deer symbolize instincts and intuition.",
+          background: "",
+          buttonText: "MINT NFTs"
         }
     ]
 
     return (
-        <>
-        <Card
+        <Grid container justifyContent="center" spacing={2} sx={{gap: "5em"}}>
+
+        {cards.map((card: cardProps) => (
+      <Card
+      key={card.title}
       sx={{
         display: "grid",
         justifyItems: "center",
@@ -25,7 +55,7 @@ const Home = () => {
     >
       <Grid display="flex" alignContent="center">
         <CardHeader
-          title={cardProps[0].title}
+          title={card.title}
           titleTypographyProps={{ align: "right" }}
           subheaderTypographyProps={{
             color: "#C5716B",
@@ -35,20 +65,12 @@ const Home = () => {
           sx={{ }}
         />
       </Grid>
-      <CardContent>
-        <Box
-          component="form"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "baseline",
-            mb: 2,
-          }}
-        >
-          <Grid container spacing={2}>
-            <CardMedia component="img" image={cardProps[0].image} width="350"></CardMedia>
+      <Grid container spacing={2} sx={{height: "200px", width: "200px"}}>
+            <CardMedia component="img" image={card.image} height="200px" width="200px" sx={{ background: card.background, borderRadius: "50%"}}></CardMedia>
           </Grid>
-        </Box>
+      <CardContent sx={{display: "flex", justifyContent: "center"}}>
+          <Typography style={{textAlign: "center"}}>{card.description}</Typography>
+
       </CardContent>
       <CardActions>
         <Button
@@ -64,12 +86,13 @@ const Home = () => {
             borderRadius: "5em",
           }}
         >
-          {cardProps[0].buttonText}
+          {card.buttonText}
         </Button>
       </CardActions>
     </Card>
+    ))}
 
-        </>
+        </Grid>
     )
 
 }
